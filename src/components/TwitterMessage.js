478cd93d4d27a4477447a55ba8a79d23ca1d1a52
Sonia Dumitru <soni.dumitru@gmail.com>
2019-06-05 15:44:15 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,28 @@
 import React from "react";
-
+//controlled component
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+handleChange = (event) => {
+  this.setState({
+    message: event.target.value
+  })
+}
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        {/*adding the attributes to the <input> element, its value should be saved in the component's state and should update on every change to the input*/}
+        <input type="text" name="message" id="message" onChange={event=>this.handleChange(event)} value={this.state.message}/>
+        {/*we have access to maxChar props from the parent, which is the max of chars => we can show remaining chars*/}
+        {this.props.maxChars - this.state.message.length}
       </div>
     );
   }
